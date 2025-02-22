19052f6bfe658fcf61e6abd5f7f638cfbf51a47c
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2025-02-22 20:12:34 +0100

    Update presets and script to setup workspace.

--- scripts/setup_workspace.sh
+++ scripts/setup_workspace.sh
@@ -4,6 +4,7 @@ sudo apt-get update
 sudo apt install ninja-build
 sudo apt-get install curl zip unzip tar
 sudo apt-get install pkg-config
+sudo apt install clang-format
 
 git submodule update --init --recursive
-cmake --workflow --preset debug
+cmake --workflow --preset configure-build-test-debug
