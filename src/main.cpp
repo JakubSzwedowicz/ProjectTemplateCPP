1f574ccd5e213c08500f937bfd7142f4ff1d1c48
Jakub Szwedowicz <kuba.szwedowicz@gmail.com>
2025-02-23 01:54:03 +0100

    Refactor main.cpp.

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,9 @@
-#include <fmt/core.h>
 #include "Calc/Calc.hpp"
+#include <fmt/core.h>
+#include <iostream>
 
-int main()
-{
-    fmt::print("Hello World!\n");
-    std::cout << Calc::Calc::templateAdd(10.0f, 5.5f) << std::endl;
-    return 0;
+int main() {
+  fmt::print("Hello World!\n");
+  std::cout << Calc::Calc::templateAdd(10.0f, 5.5f) << std::endl;
+  return 0;
 }
